bffc5be69562d7ec5c7461ab1fce3a5d88ef3aeb
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2022-06-21 13:37:46 +0545

    first commit

---
+++ backend/posts/views.py
@@ -0,0 +1,15 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(['GET'])
+def home(request):
+
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+
+    return Response(serializer.data)
+    
+
