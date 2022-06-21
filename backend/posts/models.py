48d121858135640c9e01fe6bd736360b6eaa918c
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2022-06-21 14:00:04 +0545

    migrations suck sometimes

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     details = models.TextField(blank=True)
     image = models.ImageField(default="", null=True, upload_to='post/image')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
         
     def __str__(self):
         return self.title
