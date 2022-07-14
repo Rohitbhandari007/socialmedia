8eb02130d979b516a4894343b7fce0581936cad5
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2022-07-14 18:17:09 +0545

    vews updated

--- backend/socialmedia/settings.py
+++ backend/socialmedia/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
 }
