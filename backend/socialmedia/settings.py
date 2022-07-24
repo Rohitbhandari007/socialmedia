8f9e3ff4fc0a60b6a742e522b7d30f6c5631b34b
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2022-07-24 07:46:44 +0545

    ahh

--- backend/socialmedia/settings.py
+++ backend/socialmedia/settings.py
@@ -156,3 +156,12 @@ SIMPLE_JWT = {
 
 }
 AUTH_USER_MODEL = 'users.User'
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'officalrohit69@gmail.com'
+EMAIL_HOST_PASSWORD = 'jyacotuclpcxlxua'
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from users.views import UserRegestrationView, UserLoginView, UserProfileView, FollowUnfollow, ChangePasswordView
+from users.views import UserRegestrationView, UserLoginView, UserProfileView, FollowUnfollow, UserChangePasswordView, SendPasswordResetEmailView, UserPasswordResetView
 
 urlpatterns = [
     path('register/', UserRegestrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('changepassword/', ChangePasswordView.as_view(), name='changepassword'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow-unfollow/', FollowUnfollow.as_view(), name='follow-unfollow')
+    path('follow-unfollow/', FollowUnfollow.as_view(), name='follow-unfollow'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(),
+         name='send-reset-password-email'),
+    path('reset-password/<uid>/<token>/',
+         UserPasswordResetView.as_view(), name='reset-password'),
 ]
